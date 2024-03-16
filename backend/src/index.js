85542cc97de8f0a9ae0983f577223a083912c9c9
janskibh <janski.bellon@gmail.com>
2024-03-16 14:49:13 +0000

    Initialisation

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json()); // Pour parser les requêtes JSON
+
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur Open Journal !');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Open Journal server is running on port ${PORT}`);
+});

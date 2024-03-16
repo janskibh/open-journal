7ab754735cd9c896b683f1af20bee9bba2f18451
janskibh <janski.bellon@gmail.com>
2024-03-16 16:41:47 +0000

    16/03/2024 17:41

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Open News Redaction Platform
 This platform is actually under developpement.
 
 - :~/open-journal$ ```sudo docker-compose up -d```
+- :~/open-journal$ ```sudo docker-compose up --build```
 - :~/open-journal$ ```sudo docker-compose down```
 - :~/open-journal/backend$ ```npm run index```
 - :~/open-journal/frontend$ ```npm run build```
--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,10 @@ const app = express();
 app.use(express.json()); // Pour parser les requêtes JSON
 
 app.get('/', (req, res) => {
-  res.send('Bienvenue sur Open Journal ! les amis');
+  res.send('Bienvenue sur Open Journal !');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3059;
 app.listen(PORT, () => {
   console.log(`Open Journal server is running on port ${PORT}`);
 });
